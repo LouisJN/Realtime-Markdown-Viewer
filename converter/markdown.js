bf5fd461c3a5a01bf53d543b16854b5980ac5d4f
louis <ndjomlouis@gmail.com>
2022-11-15 17:15:43 +0100

    parser

--- converter/markdown.js
+++ converter/markdown.js
@@ -1,12 +1,12 @@
- var parseHeadline = function(str) {
-   var headlineRegExp =  /^(\#{1,6})([^\#\n]+)$/m;
-   var stra = [];
-   while ((stra = headlineRegExp.exec(str)) !== null) {
-     count = stra[1].length;
-     str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
-   }
-   return str;
- }
+var parseHeadline = function (str) {
+  var headlineRegExp = /^(\#{1,6})([^\#\n]+)$/m;
+  var stra = [];
+  while ((stra = headlineRegExp.exec(str)) !== null) {
+    count = stra[1].length;
+    str = str.replace(stra[0], '<h' + count + '>' + stra[2] + '</h' + count + '>' + '\n');
+  }
+  return str;
+}
 
 var markdown = {
   parse: function (str, strict) {
@@ -15,5 +15,15 @@ var markdown = {
     return str;
   }
 };
+
+var parseHorizontaleLine = function (str) {
+  var horizontalRegExp = /^(?:([\*\-_] ?)+)\1\1$/gm;
+  var stra = [];
+  while ((stra = horizontalRegExp.exec(str)) !== null) {
+    str = str.replace(stra[0], '\n<hr/>\n');
+  }
+  return str;
+  str = parseHorizontaleLine(str);
+}
 
 module.exports = markdown;
